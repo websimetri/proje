18df48b6c3f75819c005ca87bbbfce4a60f393cb
dfoyle <damian.foyle@gmail.com>
2015-04-08 20:52:16 +0300

    Sifre unuttum icin sifre yollama on yuzu ve arayuzu tamamlandı.

--- inc/sifirla.php
+++ inc/sifirla.php
@@ -1,4 +1,5 @@
 <?php
+@include "config.php";
 /**
  * Altındaki tmpl'lerin kontrolü için değişken tanımlıyor
  * en alttaki tmpl dosyaları config içerisinde tanımlanmı
@@ -17,7 +18,22 @@ if (isset($_GET["key"]) and !empty($_GET["key"])){
 else {
     if (isset($_GET["durum"]) and $_GET["durum"] == "mail") {
 
-        $durum = "Şifre değiştirme linkiniz belirttiğiniz mail adresinze gönderilmiştir.";
+        if (Bulut::emailSorgu($_POST["fMail"])) {
+            $mail = new BulutMail($DB);
+            $yolla = $mail->sifreUnuttum($_POST["fMail"]);
+
+            if ($yolla) {
+                $durum = "Şifre değiştirme linkiniz belirttiğiniz mail adresinze gönderilmiştir.";
+            }
+            else {
+                $durum = "Mail yollama sırasında bir hata oluştu.";
+            }
+
+        }
+        else {
+            $durum = "Yanlış email girdiniz veya email'iniz sistemimizde bulunmamaktadır.";
+        }
+
         $mesaj = "<div class='mail-sifir-mesaj'>".$durum."</div>";
     }
     else {
