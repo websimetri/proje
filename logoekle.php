d7dade3f7b5d38fec84d3c1c3465733d2dce7665
Ozrc <oguzzarci@gmail.com>
2015-04-09 11:28:00 +0300

    Create logoekle.php
    Kullanıcının ekleyeceği logo.

---
+++ logoekle.php
@@ -0,0 +1,69 @@
+<?php 
+
+
+if(isset($_FILES["dosya"])) {
+
+
+	$ekle_isim = date("YmdHis");
+	$dosyaAdi = $ekle_isim."_".$_FILES["dosya"]["name"];
+
+
+
+
+
+	$izinverilenDosyalar = array("jpg","jpeg","png","gif","bmp");
+	
+	if ($dosyaHatasi = 0) {
+		echo "Dosya Yüklemesinde Bir Hata Oluştu.";
+	}else {
+
+			$dosyaTipi = $_FILES["dosya"]["type"];
+			$durum = false;
+			for ($i=0; $i < count($izinverilenDosyalar) ; $i++) { 
+				if(("image/".$izinverilenDosyalar[$i]) == $dosyaTipi){
+					$durum = true;
+					break;
+				}
+			}
+			if($durum){
+				
+				$bak = $_FILES["dosya"]["tmp_name"];
+				copy($bak,'dosya/'.$_FILES["dosya"]["name"].$dosyaAdi);
+				
+				echo "RESİM GÖNDERİMİ BAŞARILI";
+			}else {
+				echo "İZİN VERİLMEYEN DOSYA TÜRÜ";
+			}
+
+
+
+		}
+	}
+
+
+
+
+/*
+
+Hosttaki php.ini dosyasının içindeki post_max_size ve upload_max_filesize karşısındaki değerleri değiştirmemiz gerek
+daha dogrusu arttırmamız gerek
+eğer arttırmazsak;
+
+Warning: POST Content-Length of 8978294 bytes exceeds the limit of 8388608 bytes in Unknown on line 0 hatası ile karşılaşıyoruz.
+
+
+
+*/
+
+
+
+ ?>
+
+<meta charset="utf-8"/>
+ 
+ <h4>İzin Verilen Dosya Türleri : jpg , jpeg , png, gif, bmp</h4>
+ <hr style="height: 10px" >
+ <form action="" method="post" enctype="multipart/form-data">
+ 	<input type="file" name="dosya" placeholder="Dosya"/>
+ 	<input type="submit" value="Gönder"/>
+ </form>
