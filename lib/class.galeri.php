091238f37dd9552a7b60d2d8eeda4f508289c7ea
Tekin Aydoğdu <tekinaydogdu01@gmail.com>
2015-04-27 09:50:52 +0300

    düzenle fonksiyonları

--- lib/class.galeri.php
+++ lib/class.galeri.php
@@ -21,17 +21,17 @@ $pass = "";
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
 
 try {
-	$DB = new PDO($dsn, $user, $pass);
+    $DB = new PDO($dsn, $user, $pass);
 } catch (PDOException $e) {
-	echo "[HATA]: Veritabanı -" . $e->getMessage();
+    echo "[HATA]: Veritabanı -" . $e->getMessage();
 }
 
 function idEncode($id)
 {
-	// Base64 ile ID encode işlemi.
-	$enc = base64_encode($id);
+    // Base64 ile ID encode işlemi.
+    $enc = base64_encode($id);
 
-	return $enc;
+    return $enc;
 }
 
 define("UPLOAD_DIR", "upload");
@@ -46,18 +46,35 @@ define("UPLOAD_DIR", "upload");
  */
 function galeriEkle($sirketId, $isim, $aciklama = null, $aktif = "1")
 {
-	global $DB;
-	$ekle = $DB->prepare("INSERT INTO galeriler VALUES (null, :sirketId, :isim, :aciklama, :aktif)");
-	$ekle->bindParam(":sirketId", $sirketId);
-	$ekle->bindParam(":isim", $isim);
-	$ekle->bindParam(":aciklama", $aciklama);
-	$ekle->bindParam(":aktif", $aktif);
-	$ekle->execute();
-	if ($ekle->rowCount() > 0) {
-		return true;
-	} else {
-		return false;
-	}
+    global $DB;
+    $ekle = $DB->prepare("INSERT INTO galeriler VALUES (null, :sirketId, :isim, :aciklama, :aktif)");
+    $ekle->bindParam(":sirketId", $sirketId);
+    $ekle->bindParam(":isim", $isim);
+    $ekle->bindParam(":aciklama", $aciklama);
+    $ekle->bindParam(":aktif", $aktif);
+    $ekle->execute();
+    return $ekle->rowCount() > 0 ? true : false;
+}
+
+/**
+ * @param $sirketId
+ * @param $isim
+ * @param null $aciklama
+ * @param string $aktif
+ * @param $galeriId
+ * @return bool
+ */
+function galeriDuzenle($sirketId, $isim, $aciklama = null, $aktif = "1", $galeriId)
+{
+    global $DB;
+    $duzenle = $DB->prepare("UPDATE galeriler SET id_sirket = :sirketId, isim = :isim, aciklama = :aciklama, aktif = :aktif WHERE id = :galeriId");
+    $duzenle->bindParam(":sirketId", $sirketId);
+    $duzenle->bindParam(":isim", $isim);
+    $duzenle->bindParam(":aciklama", $aciklama);
+    $duzenle->bindParam(":aktif", $aktif);
+    $duzenle->bindParam(":galeriId", $galeriId);
+    $duzenle->execute();
+    return $duzenle->rowCount() > 0 ? true : false;
 }
 
 /**
@@ -66,18 +83,18 @@ function galeriEkle($sirketId, $isim, $aciklama = null, $aktif = "1")
  */
 function galeriSil($galeriId)
 {
-	global $DB;
-	$silGaleri = $DB->prepare("DELETE FROM galeriler WHERE id = :id");
-	$silGaleri->bindParam(":id", $galeriId);
-	$silGaleri->execute();
-	if ($silGaleri->rowCount() > 0) { // galeri silindiyse içindeki resimler de silinsin
-		$silResimler = $DB->prepare("DELETE FROM galeriler_resimler WHERE id_galeri = :id");
-		$silResimler->bindParam(":id", $galeriId);
-		$silResimler->execute();
-		return true; // buraya sorgu koşulu koymadım çünkü galerinin içinde resim olmayabilir
-	} else {
-		return false;
-	}
+    global $DB;
+    $silGaleri = $DB->prepare("DELETE FROM galeriler WHERE id = :id");
+    $silGaleri->bindParam(":id", $galeriId);
+    $silGaleri->execute();
+    if ($silGaleri->rowCount() > 0) { // galeri silindiyse içindeki resimler de silinsin
+        $silResimler = $DB->prepare("DELETE FROM galeriler_resimler WHERE id_galeri = :id");
+        $silResimler->bindParam(":id", $galeriId);
+        $silResimler->execute();
+        return true; // buraya sorgu koşulu koymadım çünkü galerinin içinde resim olmayabilir
+    } else {
+        return false;
+    }
 }
 
 /**
@@ -89,22 +106,29 @@ function galeriSil($galeriId)
  */
 function galeriResimEkle($galeriId, $inputname, $alt = null, $imageResize = false)
 {
-	global $DB;
+    global $DB;
     $image = ResimIslemleri::imageUpload($inputname, $imageResize);
-	if ($image[0] == true) {
-		$ekle = $DB->prepare("INSERT INTO galeriler_resimler VALUES (null, :galeriId, :url, :alt)");
-		$ekle->bindParam(":galeriId", $galeriId);
-		$ekle->bindParam(":url", $image[1]);
-		$ekle->bindParam(":alt", $alt);
-		$ekle->execute();
-		if ($ekle->rowCount() > 0) {
-			return true;
-		} else {
-			return false;
-		}
-	} else {
-		return false;
-	}
+    if ($image[0] == true) {
+        $ekle = $DB->prepare("INSERT INTO galeriler_resimler VALUES (null, :galeriId, :url, :alt)");
+        $ekle->bindParam(":galeriId", $galeriId);
+        $ekle->bindParam(":url", $image[1]);
+        $ekle->bindParam(":alt", $alt);
+        $ekle->execute();
+        return $ekle->rowCount() > 0 ? true : false;
+    } else {
+        return false;
+    }
+}
+
+function galeriResimDuzenle($galeriId, $alt = null, $resimId)
+{
+    global $DB;
+    $duzenle = $DB->prepare("UPDATE galeriler_resimler SET id_galeri = :id_galeri, alt = :alt WHERE id = :id_resim");
+    $duzenle->bindParam(":id_galeri", $galeriId);
+    $duzenle->bindParam(":alt", $alt);
+    $duzenle->bindParam(":id_resim", $resimId);
+    $duzenle->execute();
+    return $duzenle->rowCount() > 0 ? true : false;
 }
 
 /**
@@ -112,10 +136,10 @@ function galeriResimEkle($galeriId, $inputname, $alt = null, $imageResize = fals
  */
 function galeriResimSil($resimId)
 {
-	global $DB;
-	$silResimler = $DB->prepare("DELETE FROM galeriler_resimler WHERE id = :id");
-	$silResimler->bindParam(":id", $resimId);
-	$silResimler->execute();
+    global $DB;
+    $silResimler = $DB->prepare("DELETE FROM galeriler_resimler WHERE id = :id");
+    $silResimler->bindParam(":id", $resimId);
+    $silResimler->execute();
 }
 
 ?>
