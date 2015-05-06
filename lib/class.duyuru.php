8ff810776a931366308fbb39353fd4b7d5639b06
Serkan <serkan.ongan.web@gmail.com>
2015-05-06 11:08:14 +0300

    Duyuru: Sinif Uyarlamasi

--- lib/class.duyuru.php
+++ lib/class.duyuru.php
@@ -63,13 +63,79 @@ class Duyuru
      */
     public function duyuruListele($sirket_id)
     {
+        $q = "SELECT * FROM duyuru";
+        $sorgu = $this->DB->prepare($q);
+        $sorgu->execute();
+
+        $sonuclar = $sorgu->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($sonuclar) {
+            return $sonuclar;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Duyuru düzenleme işlemleri.
+     *
+     * @param $duyuru_id
+     * @param $sirket_id
+     * @param $baslik
+     * @param $detay
+     * @param $durum
+     * @return bool
+     */
+    public function duyuruDuzenle($duyuru_id, $sirket_id, $baslik, $detay, $durum)
+    {
+        $q = "
+        UPDATE duyuru
+        SET duyuru_baslik = :baslik, duyuru_detay = :detay, durum = :durum
+        WHERE id = :id AND sirket_id = :sirket
+        ";
+        $sorgu = $this->DB->prepare($q);
+        $sorgu->bindParam(":baslik", $baslik);
+        $sorgu->bindParam(":detay", $detay);
+        $sorgu->bindParam(":durum", $durum);
+        $sorgu->bindParam(":id", $duyuru_id);
+        $sorgu->bindParam(":sirket", $sirket_id);
+        $sorgu->execute();
+
+        if ($sorgu->rowCount() > 0){
+            return true;
+        }
+        else {
+            return false;
+        }
 
     }
 
+    /**
+     * Şirketin verilen ID'li duyurusunu siler.
+     *
+     * @param $duyuru_id
+     * @param $sirket_id
+     * @return bool
+     */
+    public function duyuruSil($duyuru_id, $sirket_id)
+    {
+        $q = "
+        DELETE FROM duyuru WHERE id = :id AND sirket_id = :sirket
+        ";
+        $sorgu = $this->DB->prepare($q);
+        $sorgu->bindParam(":id", $duyuru_id);
+        $sorgu->bindParam(":sirket", $sirket_id);
+        $sorgu->execute();
+
+        if ($sorgu->rowCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
-
-$x = new Duyuru();
-
-var_dump();
 
 ?>
