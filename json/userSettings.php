c02eb5d2ebfb19503e1d86aeadac8325ef83c5fd
Emre Ersöz <emreersoz55@hotmail.com>
2015-05-12 11:43:27 +0300

    KULLANICI_JSON

--- json/userSettings.php
+++ json/userSettings.php
@@ -4,25 +4,30 @@ include "../config.php";
 include "../lib/siniflar.php";
 
 
-if(isset($_GET["ref"])) {
+if(isset($_GET["ref"]) && !empty($_GET["ref"])) {
 
     $cevap = Bulut::GetSirketWithRefCode($_GET["ref"]);
     if ($cevap != false) {
         if(isset($_GET["userName"]) && isset($_GET["userSurname"])
             && isset($_GET["userMail"]) && isset($_GET["userPhone"])
-            && isset($_GET["userPass"])&& isset($_GET["userId"]) ){
+            && isset($_GET["userPass"]) && isset($_GET["userId"]) && !empty($_GET["userName"]) && !empty($_GET["userSurname"]) && !empty($_GET["userMail"]) && !empty($_GET["userPhone"]) && !empty($_GET["userPass"]) && !empty($_GET["userId"])){
             $kulbilgi = BulutJSON::kullaniciAyarlar($cevap["id"],$_GET["userId"],$_GET["userName"],$_GET["userSurname"],$_GET["userMail"],$_GET["userPhone"],$_GET["userPass"]);
             $JSON=$kulbilgi;
+            $JSON = array("durum"=>true,"mesaj"=>"Güncelleme başarıyla gerçekleşti.");
+
+        }else{
+            $JSON = array("durum"=>false,"mesaj"=>"Gerekli bilgiler eksik.");
         }
     }
     else{
-        $JSON = array("durum"=>false,"mesaj"=>"Düzenle için gerekli bilgiler eksik.");
+        $JSON = array("durum"=>false,"mesaj"=>"Referans Kodu Eksik veya Yanlış.");
     }
 }else{
-    $JSON =array( "durum"=>false,"mesaj"=>"Referans Kodu Eksik" );
+    $JSON =array( "durum"=>false,"mesaj"=>"Referans Kodu Yok" );
 }
 header('Content-Type: application/json');
 echo json_encode(array("user" => array($JSON)),JSON_PRETTY_PRINT);
+
 
 ?>
 
