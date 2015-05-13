dceab24c1ca05d32bd3a54ed5cca17670f0531a6
Oğuz Zarcı <oguzzarci@gmail.com>
2015-05-13 11:16:49 +0300

    Kategori Json

---
+++ json/companyCategory.php
@@ -0,0 +1,29 @@
+<?php
+include "../config.php";
+include "../lib/siniflar.php";
+if(isset($_GET["ref"]) && !empty($_GET["ref"]))  {
+    $cevap = Bulut::GetSirketWithRefCode($_GET["ref"]);
+
+    if ($cevap != false) {
+
+        $cevap2 = BulutJSON::getirSirketKategori($cevap["id"]);
+        $JSON = $cevap2;
+
+
+
+    }else{
+        $JSON = array("durum"=>false,"mesaj"=>"Referans Kodu Hatalı");
+    }
+
+
+}else{
+    $JSON = array("durum"=>false,"mesaj"=>"Referans Kodu Eksik");
+}
+
+header('Content-Type: application/json');
+echo json_encode(array("Kategoriler" => array($JSON)),JSON_PRETTY_PRINT);
+
+
+
+
+?>
