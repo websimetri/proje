861b89042bd37f24e92ebb48fc4a27d73c0e0736
SenSeu <proactive.yasin@gmail.com>
2015-05-14 19:24:57 +0300

    Formlar json

---
+++ json/forms.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * PARAMETRELER:
+ * =============
+ *  1. ref             Firmanın referans kodu.sirket_id
+ *  2. formId          Formun id'si
+ *
+ * *Ref ve  formId girilince DÖNENLER:
+ * =========
+ *  1. Başarılı
+ *      - formId       int         form id
+ *      - formname     string      "From Adı gönderildi."
+ *      - formhtml     string      "Form HTML gönderildi."
+ *      - formjson     bool        "Form Json gönderildi."
+ *      - date        datetime     "Form eklenme tarihi gönderildi."
+ *  2. Başarısız
+ *      - durum     bool    false
+ *      - mesaj     string  "Form Bilgileri Hatalı"
+ */
+include "../config.php";
+include "../lib/siniflar.php";
+
+
+if(isset($_GET["ref"])) {
+    //?ref kodu var mı
+    $cevap = Bulut::GetSirketWithRefCode($_GET["ref"]);
+    //Funksiyonda ref kodu getiriyor
+    $sirketId = $cevap["id"];
+    //sirket id cekilip  $sirketId ye atanıyor
+
+    if ($cevap != false) {
+        //referans kodu varsa çalısacak kısım
+
+        if (isset($_GET["formId"])) {
+            $kulBilgi = BulutJSON::getirSirketForm($_GET["formId"]);
+            //referan kodu varsa ve formId var sa çalısacak kısım
+
+
+
+            if ($kulBilgi != false) {
+                //formId var sa çalısacak kısım
+                $kulBilgi=$kulBilgi[0];
+
+                       $JSON = array("durum" => true,"mesaj" => "Giriş Başarılı", "bilgiler" => array(
+                        "formId" => $kulBilgi["id"], "formname" => $kulBilgi["adi"],
+                        "formhtml" => $kulBilgi["html"], "formjson" => $kulBilgi["json"],
+                        "date" => $kulBilgi["tarih"]));
+
+
+            } else {
+                //formId yoksa çalısacak kısım
+                $JSON = array("durum" => false, "mesaj" => "Form Bilgileri Hatalı");
+            }
+        } else {
+            //referan kodu var ve formId olmadında çalısacak kısım
+            $formlar = BulutJSON::formHepsiGetir($sirketId);
+
+
+            $bilgiler = array();
+
+            foreach ($formlar as $form){
+                $temp = array();
+                $temp["formId"] = $form["id"];
+                $temp["formname"] = $form["adi"];
+                $temp["formhtml"] = $form["html"];
+                $temp["formjson"] = $form["json"];
+                $temp["date"] = $form["tarih"];
+
+                array_push($bilgiler, $temp);
+            }
+
+            $JSON = array(
+                "durum" => true,
+                "mesaj" => "Giriş Başarılı");
+            $JSON["bilgiler"] = $bilgiler;
+
+        }
+    } else {
+        //referans kodu yanlış ise
+        $JSON = array("durum" => false, "mesaj" => "Referans Kodu Hatalı");
+    }
+}
+else{
+    //link te ?ref= yazılmadıgında çalıscak kısım
+    $JSON =array( "durum"=>false,"mesaj"=>"Referans Kodu Giriniz" );
+}
+header('Content-Type: application/json');
+echo json_encode(array("forms"=>array($JSON)), JSON_PRETTY_PRINT);
+?>
