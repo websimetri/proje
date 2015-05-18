cef9a2cc29b6bd868a2084f491d8f2ead728c101
Serkan <serkan.ongan.web@gmail.com>
2015-05-18 08:34:12 +0300

    Urun Resim: Echolar kaldirildi

--- admin/sirket/inc/urunResim.php
+++ admin/sirket/inc/urunResim.php
@@ -3,10 +3,10 @@ session_start();
 include '../../../lib/siniflar.php';
 
 if ((isset($_POST["croppedImage"]) && $_POST["croppedImage"] != "") && (isset($_SESSION["cropped"]) && $_SESSION["cropped"]["0"]["thumbnail"]) == true) {
-    var_dump($_POST);
-    echo "<br><br>";
-    var_dump($_SESSION);
-    echo "<br><br>";
+//    var_dump($_POST);
+//    echo "<br><br>";
+//    var_dump($_SESSION);
+//    echo "<br><br>";
 
 
     //resim eklem için gerekli değişkenler
