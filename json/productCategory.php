4b5b40fe9beb0a14e205e4802338ab0e428f6281
Serkan <damian.foyle@gmail.com>
2015-05-21 16:59:21 +0300

    JSON: productCategory.
    Alt kategorilerin getirilmesi eklendi. Ufak düzeltmeler yapıldı.

--- json/productCategory.php
+++ json/productCategory.php
@@ -1,22 +1,80 @@
 <?php
 include "../config.php";
 include "../lib/siniflar.php";
+
 if (isset($_GET["ref"])) {
     $cevap = Bulut::GetSirketWithRefCode($_GET["ref"]);
 
-    if ($cevap != false) {
+    if ($cevap) {
+        // Şirketin kategorileri.
 
-        $JSON = BulutJSON::getProductCategory($cevap["id"]);
+        if (isset($_GET["id"]) and !empty($_GET["id"])) {
+            // Verilen Kategori ve alt kategorileri.
+
+            // Tekil kategori bilgileri.
+            $cat = array();
+            $q = "
+            SELECT id AS categoryId, id_ust_kategori AS topCategoryId, kategori_adi AS categoryName
+            FROM kategoriler WHERE id_sirket = :sirket AND id = :id
+            ";
+            $sorgu = $DB->prepare($q);
+            $sorgu->bindParam(":sirket", $cevap["id"]);
+            $sorgu->bindParam(":id", $_GET["id"]);
+            $sorgu->execute();
+
+            $sonuclar = $sorgu->fetch(PDO::FETCH_ASSOC);
+
+            if ($sonuclar) {
+                $cat = $sonuclar;
+
+                // Alt kategoriler.
+                $q = "
+                SELECT id AS categoryId, id_ust_kategori AS topCategoryId, kategori_adi AS categoryName
+                FROM kategoriler WHERE id_sirket = :sirket AND id_ust_kategori = :id
+                ";
+                $sorgu = $DB->prepare($q);
+                $sorgu->bindParam(":sirket", $cevap["id"]);
+                $sorgu->bindParam(":id", $_GET["id"]);
+                $sorgu->execute();
+
+                $alt_kategoriler = $sorgu->fetchAll(PDO::FETCH_ASSOC);
+                $cat["subCategories"] = $alt_kategoriler;
 
 
+                $JSON = array(
+                    "durum" => true,
+                    "mesaj" => "İşlem başarılı.",
+                    "bilgiler" => $cat
+                );
+
+            }
+
+            else {
+                $JSON = array(
+                    "durum" => true,
+                    "mesaj" => "Kategori bulunamadı."
+                );
+            }
+
+
+        }
+        else {
+            $JSON = BulutJSON::getProductCategory($cevap["id"]);
+        }
 
     }else{
-        $JSON = array("durum" => false, "mesaj" => "referans kodu hatalı");
+        $JSON = array(
+            "durum" => false,
+            "mesaj" => "Referans kodunuz hatalı."
+        );
     }
 
 
 }else{
-    $JSON = array("durum" => false, "mesaj" => "referans kodu Eksik");
+    $JSON = array(
+        "durum" => false,
+        "mesaj" => "Referans kodunuz eksik."
+    );
 }
 
 header('Content-Type: application/json');
